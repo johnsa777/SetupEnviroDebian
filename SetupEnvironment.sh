97880accff406a4b2fba16c3eeb40c82d4050aed
jisbox <jisbox@30.230.138.193.client.dyn.strong-ba5.blackoakcomputers.com>
2016-08-15 22:55:01 -0400

    2n commit

--- SetupEnvironment.sh
+++ SetupEnvironment.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-#Script to setup my environment July 2016
+#Shell to setup my environment
+
 #turn color on in vim
 touch .vimrc
 chmod 770 .vimrc
 echo 'syntax on' >> .vimrc
+
 #get locate, nmap
 sudo apt-get install nmap locate -y
-#
+
+#get zsh and oh-my-zsh
+sudo apt-get install zsh -y
+sudo sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
