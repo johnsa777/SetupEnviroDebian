164f04ed1f72bbaef68d6d5c3b9ad95a0afe9b1f
jisbox <jisbox@JSs-Macbox.local>
2016-08-24 23:03:02 -0400

    minor changes

--- SetupEnvironment.sh
+++ SetupEnvironment.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 #Shell to setup my environment
 #This setup is appropriate for ephemeral sessions on google cloud services
-
-#Change to root
-su root
-cd ~
-
-#turn color on in vim if there are no vim settings
-if [ ! -f .vimrc ]; then
-touch .vimrc
-chmod 770 .vimrc
-echo 'syntax on' >> .vimrc
+#Turn color n on in vim if there are no vim settings
+if [ ! -f /root/.vimrc ]; then
+    touch /root/.vimrc
+    chmod 770 /root/.vimrc
+    echo 'syntax on' >> /root/.vimrc
 fi
-
 #get locate, nmap
 sudo apt-get install nmap locate -y
 
 #get zsh and oh-my-zsh
+sudo rm -r /root/.oh-my-zsh; echo '*** delete prev oh-my-zsh install if present'
 sudo apt-get install zsh -y
 sudo sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 #dowload Honukai theme and reloading shell
 #wget https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm/master/honukai.zsh-theme
 #mv honukai.zsh-theme /home/$USER/.oh-my-zsh/themes/
-sed -i -e 's/robbyrussell/honukai/' .zshrc  #set theme
+echo '*** setting template'
+sed -i -e 's/robbyrussell/honukai/' /root/.zshrc  #set theme
 source .zshrc #reloading shell settings
