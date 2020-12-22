0bac7938cb7cec3dd469030c7401ab38455913ae
JohnS <sailingtheseven@gmail.com>
2020-12-22 17:26:25 +0700

    Update SetupEnvironment.sh
    Adding color and new features

--- SetupEnvironment.sh
+++ SetupEnvironment.sh
@@ -1,20 +1,62 @@
 #!/bin/bash
 #Shell to setup Debian environment
+#https://github.com/johnsa777/SetupEnviroDebian
 #This setup is appropriate for ephemeral sessions or new systems
+#To run make sure you make the script executable "chmod 755 SetupEnviroDebian.sh"
+#Run with sudo
 
-#Change system password
+#Setting up terminal color and saying hello
+RESET="\033[0m"
+GREEN="\033[42m"
+echo $(echo -e $GREEN"HI, this is a script that sets up a Debian environment"$RESET);
+
+#Change system name
 while true; do
-    read -p "Would you like to change your user password? (y/n)" yn
+    read -p "$(echo -e $GREEN"Would you like to change machine name (y/n)"$RESET)" yn
     case $yn in
-        [Yy]* ) sudo passwd; break;;
+        [Yy]* ) 
+            read -p "$(echo -e $GREEN"Enter machine name"$RESET)" MachineName
+            sudo hostnamectl set-hostname $MachineName
+            break;;
         [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
+        * ) echo $(echo -e $GREEN"Please answer yes or no."$RESET);;
     esac
 done
 
+#Change system password
+while true; do
+    read -p "$(echo -e $GREEN"Would you like to change your user password? (y/n)"$RESET)" yn
+    case $yn in
+        [Yy]* ) sudo passwd; break;;
+        [Nn]* ) break;;
+        * ) echo $(echo -e $GREEN"Please answer yes or no."$RESET);;
+    esac
+done
+
+#setup cron job to update system continuously 
+while true; do
+    read -p "$(echo -e $GREEN"Would you like to update system automatically every day? (y/n)"$RESET)" Update
+    case $Update in
+        [Yy]* ) CronOn=1; break;;
+        [Nn]* ) CronOn=0; break;;
+        * ) echo $(echo -e $GREEN"Please answer yes or no."$RESET);;
+    esac
+done
+
+if [[ "$CronOn" -eq 1 ]];then
+    if (!(crontab -l | grep -q 'apt update')); then
+            crontab -l > mycron
+            #echo new cron into cron file
+            echo "08 04 * * * apt update -y;apt dist-upgrade -y;apt clean -y" >> mycron
+            #install new cron file
+            sudo crontab mycron
+            rm mycron
+    fi
+fi
+
 #Redo ssh keys
 while true; do
-    read -p "Would you like to regenerate your ssh keys (y/n)" yn
+    read -p "$(echo -e $GREEN"Would you like to regenerate your ssh keys (y/n)"$RESET)" yn
     case $yn in
         [Yy]* ) sudo ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 521 -y; break;;
         [Nn]* ) break;;
@@ -22,25 +64,20 @@ while true; do
     esac
 done
 
-#Remove identification banner
+#Removing identification banner
+echo $(echo -e $GREEN"Removing identification banner"$RESET);
 sudo rm /etc/issue
 sudo rm /etc/motd
-#update system
+#Updating system
+echo $(echo -e $GREEN"Updating system"$RESET);
 sudo apt update -y
 sudo apt dist-upgrade -y
-#install software
+#Installing nmap, vim, htop
+echo $(echo -e $GREEN"Installing nmap, vim, htop"$RESET);
 sudo apt install nmap locate vim -y
 sudo apt install htop -y
-#setup cron job to update system continuously 
-if !(crontab -l | grep -q 'apt update'); then
-        crontab -l > mycron
-        #echo new cron into cron file
-        echo "08 04 * * * apt update -y;apt dist-upgrade -y;apt clean -y" >> mycron
-        #install new cron file
-        sudo crontab mycron
-        rm mycron
-fi
-#Turn color on in vim
+#Turning color on in vim
+echo $(echo -e $GREEN"Turning color on in vim"$RESET);
 if (grep -q "syntax on" "/root/.vimrc") 
 then
         echo 'vim settings present'
@@ -49,10 +86,11 @@ else
         sudo chmod 770 /root/.vimrc
         sudo echo "syntax on" >> /root/.vimrc
 fi
-#get zsh and oh-my-zsh
+#Installing zsh and oh-my-zsh
+echo $(echo -e $GREEN"Installing zsh and oh-my-zsh"$RESET);
 if [ -e /root/.oh-my-zsh ]
 then
-        echo 'zsh present'
+        echo $(echo -e $GREEN"zsh already present"$RESET);
 else
         sudo apt install zsh -y
         sudo sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -60,4 +98,5 @@ else
         sed -i -e 's/robbyrussell/maran/' /root/.zshrc  #set theme
         source .zshrc #reloading shell settings
 fi
-echo 'END OF SCRIPT'
+#Done
+echo $(echo -e $GREEN"DONE"$RESET);
